60d89a94f46e11e61982cde7816b9555a85da14a
olha <olha>
2009-12-05 19:12:48 +0200

    New version is 0.1.00062.00000

--- vercontrol.h
+++ vercontrol.h
@@ -1,7 +1,7 @@
 #define ProjectName VersionStorage
 #define VMAJOR 0
 #define VMINOR 1
-#define VSUBMINOR 00061
+#define VSUBMINOR 00062
 #define VHFNUMBER 00000
-#define X_VER_STRING 0.1.00061.00000
+#define X_VER_STRING 0.1.00062.00000
 #define X_COPYRIGHT "(C) 2021 by X, Inc. All rights reserved."
