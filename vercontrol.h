cee6b2f67db7913a8f7875fd1465fe467a7eff0a
kolesnikovaO <32612936+kolesnikovaO@users.noreply.github.com>
2021-12-01 11:00:28 +0200

    Create vercontrol.h

---
+++ vercontrol.h
@@ -0,0 +1,7 @@
+#define ProjectName VersionStorage
+#define VMAJOR 0
+#define VMINOR 1
+#define VSUBMINOR 00001
+#define VHFNUMBER 00000
+#define X_VER_STRING 0.1.00001.00000
+#define X_COPYRIGHT "(C) 2021 by X, Inc. All rights reserved."
