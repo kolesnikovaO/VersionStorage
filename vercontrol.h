fc2a5c297a4c6e58c216d580471afd87af643b63
olha <olha>
2021-12-07 17:39:26 +0200

    New version is $(<../tempf)

--- vercontrol.h
+++ vercontrol.h
@@ -1,7 +1,7 @@
 #define ProjectName VersionStorage
 #define VMAJOR 0
 #define VMINOR 1
-#define VSUBMINOR 00069
+#define VSUBMINOR 00070
 #define VHFNUMBER 00000
-#define X_VER_STRING 0.1.00069.00000
+#define X_VER_STRING 0.1.00070.00000
 #define X_COPYRIGHT "(C) 2021 by X, Inc. All rights reserved."
